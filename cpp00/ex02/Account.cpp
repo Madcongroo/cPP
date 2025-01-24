9b2bdad177bd0ef96c492e1d96bf1efe245ec40e
Baptiste Proton <bproton@c2r2s3.42lausanne.ch>
2025-01-24 14:37:44 +0100

    cpp01 debut

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -3,13 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: proton <proton@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bproton <bproton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 14:41:25 by proton            #+#    #+#             */
-/*   Updated: 2025/01/23 14:41:37 by proton           ###   ########.fr       */
+/*   Updated: 2025/01/24 14:21:03 by bproton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
+Account::Account( int initial_deposit )
+{
+    
+}
 
+Account::Account( void )
+{
+    
+}
+
+Account::~Account( void )
+{
+    
+}
+
+void Account::displayAccountsInfos( void )
+{
+    
+}
+
+void    Account::displayStatus( void ) const
+{
+    
+}
