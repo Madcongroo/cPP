62b841b7331324ddca64b662787f2c4dd80afb76
proton <b.proton@hotmail.com>
2025-02-05 12:40:08 +0100

    cpp02 starting

---
+++ cpp02/Fixed.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: proton <proton@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 12:33:05 by proton            #+#    #+#             */
+/*   Updated: 2025/02/05 12:38:00 by proton           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <string>
+
+class Fixed
+{
+	public:
+		Fixed( int fixed_point_number );
+		Fixed( )
+		int	getRawBits( void );
+};
+
+#endif
