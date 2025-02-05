1ad7eb5b1d096e96a0bf44f3fb39a08d7789a8ae
Baptiste Proton <bproton@c2r5s8.42lausanne.ch>
2025-02-05 16:05:14 +0100

    start ex00

--- cpp02/Fixed.hpp
+++ cpp02/Fixed.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: proton <proton@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bproton <bproton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 12:33:05 by proton            #+#    #+#             */
-/*   Updated: 2025/02/05 14:20:49 by proton           ###   ########.fr       */
+/*   Updated: 2025/02/05 16:02:55 by bproton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,16 @@
 class Fixed
 {
 	public:
-		Fixed( int fixed_point_number );
-		Fixed(  )
-		int	getRawBits( void );
+		Fixed();
+		Fixed(  );
+		~Fixed();
+
+		int		getRawBits( void ) const; //renvoie la valeur du nombre a virgule fixe sans la convertir
+		void	setRawBits( int const raw ); //initialise la valeur du nombre avec celle passee en parametre
+
+	private:
+		int					_fixed_point;
+		static const int	_bits_nbr;
 };
 
 #endif
--- cpp02/main.cpp
+++ cpp02/main.cpp
@@ -3,10 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: proton <proton@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bproton <bproton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 12:33:08 by proton            #+#    #+#             */
-/*   Updated: 2025/02/05 12:33:08 by proton           ###   ########.fr       */
+/*   Updated: 2025/02/05 15:09:47 by bproton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int main( void )
+{
+	Fixed	a;
+	Fixed	b( a );
+	Fixed	c;
+	
+	c = b;
+	
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	
+	return 0;
+}
